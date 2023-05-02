92eb8193af63130eca8c9db04ac99492b004e01c
brandonzane <bmushori@gmail.com>
2023-05-02 08:27:42 +0200

    Initial Commit

---
+++ screens/Home.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { View, ScrollView, Text, SafeAreaView, StyleSheet } from "react-native";
+
+import 
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
